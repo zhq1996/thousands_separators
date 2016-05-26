29eccd1b9bf245a81883bc3169c5622aaaea4671
zhq1996 <1553762217@qq.com>
2016-05-26 10:27:26 +0800

    Add files via upload

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,32 @@
 'use strict';
-
 function thousands_separators(num) {
-  
-}
+    var num = num.toString();
+    var str=[];
+    var panduan;
+    var result;
 
+    panduan = num.indexOf(".");
+    if (panduan===-1) {
+        str.push(num);
+    } else {
+        str=num.split(".");
+    }
+    if (str[0].length<= 3) {
+        result=str[0];
+    } else {
+        var weizhi=str[0].length % 3;
+        result=str[0].slice(0, weizhi)+',';
+        while (str[0].length - weizhi>3) {
+            result+=str[0].slice(weizhi, weizhi + 3) + ',';
+            weizhi+=3;
+        }
+        result+=str[0].slice(weizhi);
+    }
+    if (str.length > 1) {
+        result+='.';
+        result+=str[1];
+    }
+
+    return result;
+}
 module.exports = thousands_separators;
